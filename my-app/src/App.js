d2a14dbe01f7db82adc5111e2ba321f42f921de9
LHHDANG <142959277+LHHDANG@users.noreply.github.com>
2023-11-24 12:02:11 +0700

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import PricingPage from './components/PricingPage';
 import ItemList from './components/ItemList';
+import ProductList from './components/ProductList';
+import './css/styles.css'
 
 export default function MyApp() {
   return (
     <div className='App'>
-      <ItemList />
+      {/* <ItemList /> */}
+      <ProductList/>
     </div>
   )
 }
