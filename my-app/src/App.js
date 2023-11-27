9c8331d05a8b57963525a9b6a54d21dc519823d7
LHHDANG <142959277+LHHDANG@users.noreply.github.com>
2023-11-27 09:23:26 +0700

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,34 @@ import './App.css';
 import PricingPage from './components/PricingPage';
 import ItemList from './components/ItemList';
 import ProductList from './components/ProductList';
-import './css/styles.css'
+import './css/styles.css';
 
-export default function MyApp() {
+// export default function MyApp() {
+//   return (
+//     <div className='App'>
+//       {/* <ItemList /> */}
+//       {/* <ProductList/> */}
+//       {/* <PricingPage/> */}
+//     </div>
+//   )
+// }
+
+export default function Counter() {
+  let like = 10;
   return (
-    <div className='App'>
-      {/* <ItemList /> */}
-      {/* <ProductList/> */}
-      {/* <PricingPage/> */}
-    </div>
-  )
+      <div>
+          {/* camelcase */}
+          <button
+          onClick={ () => {
+              like++;
+              console.log(like);
+          }}
+      >
+          Like
+      </button>
+      <h1>{like}</h1>
+      <h1>{like}</h1>
+      <h1>{like}</h1>
+      </div>
+  );
 }
---
+++ my-app/src/codeSnippet/ss4/App.js
@@ -0,0 +1,24 @@
+function ClickMe() {
+    return <button onClick={ () => console.log("Clicked 👆")}>Click me</button>;
+}
+
+function Counter() {
+    let like = 10;
+    return (
+        <div>
+            {/* camelcase */}
+            <button
+            onClick={ () => {
+                like++;
+                console.log(like);
+            }}
+        >
+            Like
+        </button>
+        <h1>{like}</h1>
+        <h1>{like}</h1>
+        <h1>{like}</h1>
+        </div>
+    );
+}
+
